b27affbe4dcaafc8668575aced91e971ee07cca6
Matt Dudzik <mdudzik92@gmail.com>
2021-07-29 01:14:36 -0500

    Adding pixels to input values with handleUpdate() function and changing them via an event listener on mousemove or changing the ranges or color

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
-	console.log(this.value);
+	// '' sets it to empty otherwise would return undefined with the colors
+	const suffix = this.dataset.sizing || "";
+	// Accessing the HTML(document), going into the root of the document (main HTML) with documentElement, style (which selects its CSS)
+	// Then grabbing the property by it's name (blur, spacing, base), and putting together it's value and data-sizing values.
+	// This appends pixels to the values so that they change when the function is called.
+	document.documentElement.style.setProperty(
+		`--${this.name}`,
+		this.value + suffix,
+	);
 }
 
-inputs.forEach((input) => input.addEventListener("change", handleUpdate));
 // Triggers the changes
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
 inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
